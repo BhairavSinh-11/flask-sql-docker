ad0c80bb4cd0e1e73469fa40d46e511b11a25aa1
Bhairav Sinh <dbhairavsinh2423@gmail.com>
2026-05-15 18:02:12 +0530

    fixed issue

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
     MYSQL_USER = os.getenv("MYSQL_USER", "root")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "flaskdb")
+    MYSQL_DB = os.getenv("MYSQL_DATABASE", "flaskdb")
     
     MYSQL_CURSORCLASS = 'DictCursor'
